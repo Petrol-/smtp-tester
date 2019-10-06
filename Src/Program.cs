512e3fae7203597558f84a7fc09da99e7f34d595
Nicolas HAHN <nicolas.hahn@unilor.fr>
2019-10-06 16:33:52 +0200

    initial commit

---
+++ Src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SmtpTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
